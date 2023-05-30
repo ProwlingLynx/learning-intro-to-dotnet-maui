687f658d1104a68dee4a60cb845b810ab88550cd
Javier Tamez <tamezjavier@gmail.com>
2023-05-29 23:36:13 -0500

    Added event handler in MainViewModel.cs.

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+
+namespace MauiApp1.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        string text;
+        public string Text
+        {
+            get { return text; }
+            set
+            {
+                text = value;
+                OnPropertyChanged(nameof(Text));
+                // nameof grabs the name of the variable/class/input. It does not give values.
+                // eg. string cat = "meow"; nameof(cat) -> "cat" not "meow".
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged; // Its a field that needs to be implemented from INotifyPropertyChanged.
+        // PropertyChangedEventHandler is an event handler object
+        void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
